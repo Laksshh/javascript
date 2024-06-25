cb14e0e8c50b05ea8f3c2d5c538dba7a41c070c2
Laksshh <165757799+Laksshh@users.noreply.github.com>
2024-06-25 17:16:45 +0530

    syntax , declared variables and datatypes

---
+++ main.js
@@ -0,0 +1,46 @@
+let abc = null;
+// let keyword is used for block variable
+let firstName = "laksh";
+let lastName = "Soni";
+
+let fullName = firstName +" "+ lastName;
+console.log(fullName) // Lakshya Soni
+if (true){
+   let lastName = "Goyal";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName) // Lakshya Goyal
+}
+console.log(fullName) // Lakshya Soni
+
+// var keywoed is used for global variable
+var x = 4;
+var y = 6;
+var z = x +y;
+
+console.log(z); // 10
+if (true){
+var z = 11;
+console.log(z); // 11
+}
+console.log(z); // 10
+
+
+let myName = "Lakshya"
+console.log(typeof myName) // number
+
+let a;
+console.log( typeof a); // undefined
+
+let b=null;
+console.log(typeof b); // object
+
+let c = NaN;
+console.log(typeof c); // Nan
+
+let d = 
+
+
+
+
+
